a68f8b97b7e423a972ad20f84fb5e30d9de0ed0d
isaackogan <isaacikogan@gmail.com>
2024-02-24 14:50:11 -0500

    Update messaging

--- strike-app/src/components/pages/Home/Home.tsx
+++ strike-app/src/components/pages/Home/Home.tsx
@@ -40,9 +40,10 @@ export default function Home() {
                     student-made to help other students navigate this confusing time.
                 </p>
                 <p>
-                    In the event of a strike, YorkU will publish a list of active classes, tutorials and labs,
-                    as well as provide information on all eClass course pages, making this app effectively redundant. Please check your e-mail
-                    before Monday as professors will communicate their plans with you.
+                    YorkU has also released an <PLink href={"https://coursestatus.yorku.ca/"}>official tool</PLink> to determine if your course is
+                    affected by the strike. This tool takes manual input from professors, so expect <strong>about a week</strong>, starting from Mon, Feb 26th, 2024,
+                    before it is accurate to all your courses. There has also been a preparation in eClass to notify you if your course is affected, once again starting Monday.
+                    Please check your e-mail on Mon, Feb 26th, 2024, as professors will communicate their plans with you.
                 </p>
                 <StrikeForm />
             </DescriptionBox>
